417411c07f6d432b72af8b43d933b3e800a95a95
ABANGPETER2000 <114669610+ABANGPETER2000@users.noreply.github.com>
2024-06-14 16:18:24 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,27 +1,10 @@
-//
-// document.querySelector('#dropdownMenuButton').addEventListener('click', () => {
-//   if (document.querySelector('#btn-img').classList.contains('fa-message')) {
-//     document.querySelector('#btn-img').classList.remove('fa-message');
-//     document.querySelector('#btn-img').classList.add('fa-xmark');
-//     document.querySelector('#dropdownMenuButton').classList.remove('shade');
-//   } else {
-//     document.querySelector('#btn-img').classList.remove('fa-xmark');
-//     document.querySelector('#btn-img').classList.add('fa-message');
-//     document.querySelector('#dropdownMenuButton').classList.add('shade');
-//   }
-//
-// });
-//
 
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
   'use strict'
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
   var forms = document.querySelectorAll('.needs-validation')
 
-  // Loop over them and prevent submission
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
       form.addEventListener('submit', function (event) {
@@ -35,7 +18,6 @@
     })
 })()
 
-// Custom email validation
   document.getElementById('floatingEmail').addEventListener('input', function() {
     const emailInput = this.value;
     const allowedDomain = ".com";
